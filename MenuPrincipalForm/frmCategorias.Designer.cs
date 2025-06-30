5d33a11bd9fe300fd03320a38c95468237f3817d
Sant1Franco <francosantiago312@gmail.com>
2025-06-30 01:02:04 -0300

    Cambios en los formularios Editoriales y Categorias

--- MenuPrincipalForm/frmCategorias.Designer.cs
+++ MenuPrincipalForm/frmCategorias.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmCategorias";
+            label1 = new Label();
+            label2 = new Label();
+            txtIdCategoria = new TextBox();
+            txtNombreCategoria = new TextBox();
+            btnAgregar = new Button();
+            btnModificar = new Button();
+            btnEliminar = new Button();
+            btnLimpiar = new Button();
+            dgvCategorias = new DataGridView();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvCategorias).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(63, 204);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 28);
+            label1.TabIndex = 0;
+            label1.Text = "ID Categoría";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(63, 305);
+            label2.Name = "label2";
+            label2.Size = new Size(175, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre Categoría";
+            // 
+            // txtIdCategoria
+            // 
+            txtIdCategoria.Font = new Font("Segoe UI", 11F);
+            txtIdCategoria.Location = new Point(415, 200);
+            txtIdCategoria.Name = "txtIdCategoria";
+            txtIdCategoria.Size = new Size(358, 32);
+            txtIdCategoria.TabIndex = 2;
+            // 
+            // txtNombreCategoria
+            // 
+            txtNombreCategoria.Font = new Font("Segoe UI", 11F);
+            txtNombreCategoria.Location = new Point(415, 305);
+            txtNombreCategoria.Name = "txtNombreCategoria";
+            txtNombreCategoria.Size = new Size(358, 32);
+            txtNombreCategoria.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Font = new Font("Segoe UI", 12F);
+            btnAgregar.Location = new Point(63, 444);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(150, 50);
+            btnAgregar.TabIndex = 4;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnModificar
+            // 
+            btnModificar.Font = new Font("Segoe UI", 12F);
+            btnModificar.Location = new Point(306, 444);
+            btnModificar.Name = "btnModificar";
+            btnModificar.Size = new Size(150, 50);
+            btnModificar.TabIndex = 5;
+            btnModificar.Text = "Modificar";
+            btnModificar.UseVisualStyleBackColor = true;
+            btnModificar.Click += btnModificar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Font = new Font("Segoe UI", 12F);
+            btnEliminar.Location = new Point(547, 444);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(150, 50);
+            btnEliminar.TabIndex = 6;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Font = new Font("Segoe UI", 12F);
+            btnLimpiar.Location = new Point(783, 444);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(150, 50);
+            btnLimpiar.TabIndex = 7;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // dgvCategorias
+            // 
+            dgvCategorias.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCategorias.Location = new Point(63, 642);
+            dgvCategorias.Name = "dgvCategorias";
+            dgvCategorias.RowHeadersWidth = 51;
+            dgvCategorias.Size = new Size(870, 441);
+            dgvCategorias.TabIndex = 10;
+            dgvCategorias.CellContentClick += dgvCategorias_CellContentClick;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 30F);
+            label3.Location = new Point(63, 55);
+            label3.Name = "label3";
+            label3.Size = new Size(863, 67);
+            label3.TabIndex = 12;
+            label3.Text = "Bienvenido al fomulario de Categorias";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // frmCategorias
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(998, 1180);
+            Controls.Add(label3);
+            Controls.Add(dgvCategorias);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnModificar);
+            Controls.Add(btnAgregar);
+            Controls.Add(txtNombreCategoria);
+            Controls.Add(txtIdCategoria);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmCategorias";
+            Text = "frmCategorias";
+            ((System.ComponentModel.ISupportInitialize)dgvCategorias).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtIdCategoria;
+        private TextBox txtNombreCategoria;
+        private Button btnAgregar;
+        private Button btnModificar;
+        private Button btnEliminar;
+        private Button btnLimpiar;
+        private DataGridView dgvCategorias;
+        private Label label3;
     }
 }
--- MenuPrincipalForm/frmEditoriales.Designer.cs
+++ MenuPrincipalForm/frmEditoriales.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmEditoriales";
+            txtIdEditorial = new TextBox();
+            txtNombreEditorial = new TextBox();
+            btnAgregar = new Button();
+            btnModificar = new Button();
+            btnEliminar = new Button();
+            btnLimpiar = new Button();
+            label3 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            dgvEditoriales = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvEditoriales).BeginInit();
+            SuspendLayout();
+            // 
+            // txtIdEditorial
+            // 
+            txtIdEditorial.Font = new Font("Segoe UI", 11F);
+            txtIdEditorial.Location = new Point(433, 195);
+            txtIdEditorial.Name = "txtIdEditorial";
+            txtIdEditorial.Size = new Size(358, 32);
+            txtIdEditorial.TabIndex = 0;
+            // 
+            // txtNombreEditorial
+            // 
+            txtNombreEditorial.Font = new Font("Segoe UI", 11F);
+            txtNombreEditorial.Location = new Point(433, 300);
+            txtNombreEditorial.Name = "txtNombreEditorial";
+            txtNombreEditorial.Size = new Size(358, 32);
+            txtNombreEditorial.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Font = new Font("Segoe UI", 12F);
+            btnAgregar.Location = new Point(64, 456);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(150, 50);
+            btnAgregar.TabIndex = 2;
+            btnAgregar.Tag = "";
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnModificar
+            // 
+            btnModificar.Font = new Font("Segoe UI", 12F);
+            btnModificar.Location = new Point(288, 456);
+            btnModificar.Name = "btnModificar";
+            btnModificar.Size = new Size(150, 50);
+            btnModificar.TabIndex = 3;
+            btnModificar.Text = "Modificar";
+            btnModificar.UseVisualStyleBackColor = true;
+            btnModificar.Click += btnModificar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Font = new Font("Segoe UI", 12F);
+            btnEliminar.Location = new Point(530, 456);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(150, 50);
+            btnEliminar.TabIndex = 4;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Font = new Font("Segoe UI", 12F);
+            btnLimpiar.Location = new Point(784, 456);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(150, 50);
+            btnLimpiar.TabIndex = 5;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 30F);
+            label3.Location = new Point(64, 54);
+            label3.Name = "label3";
+            label3.Size = new Size(870, 67);
+            label3.TabIndex = 11;
+            label3.Text = "Bienvenido al fomulario de Editoriales ";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(64, 195);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 28);
+            label1.TabIndex = 12;
+            label1.Text = "ID Editorial";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(64, 297);
+            label2.Name = "label2";
+            label2.Size = new Size(163, 28);
+            label2.TabIndex = 13;
+            label2.Text = "Nombre Editorial";
+            // 
+            // dgvEditoriales
+            // 
+            dgvEditoriales.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEditoriales.Location = new Point(64, 680);
+            dgvEditoriales.Name = "dgvEditoriales";
+            dgvEditoriales.RowHeadersWidth = 51;
+            dgvEditoriales.Size = new Size(870, 399);
+            dgvEditoriales.TabIndex = 14;
+            dgvEditoriales.CellContentClick += dgvEditoriales_CellContentClick;
+            // 
+            // frmEditoriales
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(998, 1180);
+            Controls.Add(dgvEditoriales);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnModificar);
+            Controls.Add(btnAgregar);
+            Controls.Add(txtNombreEditorial);
+            Controls.Add(txtIdEditorial);
+            Name = "frmEditoriales";
+            Text = "frmEditoriales";
+            ((System.ComponentModel.ISupportInitialize)dgvEditoriales).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtIdEditorial;
+        private TextBox txtNombreEditorial;
+        private Button btnAgregar;
+        private Button btnModificar;
+        private Button btnEliminar;
+        private Button btnLimpiar;
+        private Label label3;
+        private Label label1;
+        private Label label2;
+        private DataGridView dgvEditoriales;
     }
 }
